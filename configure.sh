a3915ec04aa550b2480b8536efc126d0d60f554d
Yash Katta <yash.katta1@gmail.com>
2025-04-03 16:15:01 +0530

    Added GLFW (1)

---
+++ configure.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+cmake -S . -B build  -DGLFW_BUILD_DOCS=OFF
--- main.cc
+++ main.cc
@@ -1,8 +1,62 @@
 #include <iostream>
 
-int main() {
+#include <GLFW/glfw3.h>
+
+static void error_callback(int error, const char *description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main()
+{
+
+	GLFWwindow *window;
 
 	std::cout << "Hi!" << std::endl;
+
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+
+	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(640, 480, "Test", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwSetKeyCallback(window, key_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// int width, height;
+		// glfwGetFramebufferSize(window, &width, &height);
+		// const float ratio = width / (float)height;
+
+		// glViewport(0, 0, width, height);
+		// glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		// glfwWaitEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 
 	return 0;
 }
