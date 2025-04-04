e1795d90cf47b5f1fdac19548c49980eb59e2a95
Yash Katta <yash.katta1@gmail.com>
2025-04-04 16:36:20 +0530

    Added Triangles, Shaders, GL fill & line

--- main.cc
+++ main.cc
@@ -3,6 +3,49 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+/* Structs */
+typedef struct vertex
+{
+	float v[3];
+} vertex;
+
+/* Global & Static */
+static const vertex vertices[4] = {
+		{0.5f, 0.5f, 0.0f},
+		{0.5f, -0.5f, 0.0f},
+		{-0.5f, -0.5f, 0.0f},
+		{-0.5f, 0.5f, 0.0f}};
+
+static const int indices[] = {
+		0, 1, 3, // First triangle
+		1, 2, 3	 // Second triangle
+};
+
+static bool wireframe = false;
+
+static const char *vertex_shader_text =
+		"#version 420\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"out vec3 color;\n"
+		"\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"    gl_Position = vec4(aPos, 1.0);\n"
+		"    color = vec3(0.8, 0.2, 0.0);\n"
+		"}\n";
+
+static const char *fragment_shader_text =
+		"#version 420\n"
+		"in vec3 color;\n"
+		"out vec4 oFragment;\n"
+		"\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"    oFragment = vec4(color, 1.0);\n"
+		"}\n";
+
 static void error_callback(int error, const char *description)
 {
 	fprintf(stderr, "Error: %s\n", description);
@@ -12,6 +55,34 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	if (key == GLFW_KEY_M && action == GLFW_PRESS)
+		wireframe = !wireframe;
+}
+
+void checkCompileErrors(unsigned int shader, std::string type)
+{
+	int success;
+	char infoLog[1024];
+	if (type != "PROGRAM")
+	{
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n"
+								<< infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+	else
+	{
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n"
+								<< infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
 }
 
 int main()
@@ -29,6 +100,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
 	window = glfwCreateWindow(640, 480, "Test", NULL, NULL);
 	glfwMakeContextCurrent(window);
@@ -38,14 +110,51 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
 		std::cout << "Failed to initilize GLAD" << std::endl;
 		exit(EXIT_FAILURE);
 	}
 
 	glfwSetKeyCallback(window, key_callback);
 
-	glClearColor(0.22f, 0.83f, 0.86f, 0.0f);
+	const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+	glCompileShader(vertex_shader);
+	checkCompileErrors(vertex_shader, "VERTEX");
+
+	const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+	glCompileShader(fragment_shader);
+	checkCompileErrors(fragment_shader, "FRAGMENT");
+
+	const GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+	checkCompileErrors(program, "PROGRAM");
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	std::cout << sizeof(vertices) << std::endl;
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	std::cout << sizeof(indices) << std::endl;
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	std::cout << sizeof(indices) << std::endl;
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void *)0);
+	glEnableVertexAttribArray(0);
+
+	glClearColor(0.2f, 0.4f, 0.86f, 0.0f);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -56,10 +165,26 @@ int main()
 		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		if (wireframe)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		else
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+		glUseProgram(program);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		// glDrawArrays(GL_POINTS, 0, 4);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 		// glfwWaitEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(program);
 
 	glfwDestroyWindow(window);
 
